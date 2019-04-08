bbdb7aae3daac681555141cae44d52c291baaff6
Attila Ambrus <attila.ambrus@xapt.com>
2019-04-08 10:01:00 +0200

    Implement BaseModel protocol

--- NMDEF.Sync/Classes/Abstracts/BaseEntity.swift
+++ NMDEF.Sync/Classes/Abstracts/BaseEntity.swift
@@ -1,7 +1,8 @@
 import EVReflection
 import RealmSwift
+import NMDEF_Base
 
-open class BaseEntity: BaseObject {
+open class BaseEntity: BaseObject, BaseModel {
     @objc public dynamic var id = ""
     @objc public dynamic var version = ""
     @objc public dynamic var deleted = false
